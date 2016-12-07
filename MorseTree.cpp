d9dfb14a4c7c6136fb3f7bade59da43a909414f3
lgilsinger <lagnr4@mail.umkc.edu>
2016-12-06 20:29:29 -0600

    Update MorseTree.cpp

--- MorseTree.cpp
+++ MorseTree.cpp
@@ -7,13 +7,49 @@
 //constructor
 MorseTree::MorseTree()
 {
-    root = new node;
-    root->data = "";
-    root->code = "";
-    root->left = NULL;
-    root->right = NULL:
+	//create new tree and set everything to null
+	root = new node;
+	root->data = "";
+	root->morseCode = "";
+	root->left = NULL;
+	root->right = NULL:
 }
 
-void MorseTree::BuildTree(char letter, string code){
+void MorseTree::BuildTree(char letter, string code) {
+
+	morsify[letter] = code;
+	node *ptr = root;
+	ptr->data = letter;
+
+	for (int i = 0; i <= code.length(); i++)
+	{
+		if (code[i] = '.')
+		{
+			if (ptr->left == nullptr)
+			{
+				ptr->left = new node;
+				ptr = ptr->left;
+			}
+			else
+			{
+				ptr = (node = node->left);
+			}
+		}
+		else if (code[i] = '-')
+		{
+			if (ptr->right == nullptr)
+			{
+				ptr->right = new node;
+				ptr = ptr->right;
+			}
+			else
+			{
+				ptr = (node = node->right);
+			}
+		
+		}
+	}
+	ptr->morseCode = code;
+	
 
 }
